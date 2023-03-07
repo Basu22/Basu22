ae54101f205222f4a5ec73899ec15c8fd1ba0621
Basilio Ossvald <basilio.ossval@gmail.com>
2023-03-07 18:45:24 -0300

    url imagen menu mobile

--- js/eventoMenu.js
+++ js/eventoMenu.js
@@ -7,14 +7,14 @@ iconoHamb.addEventListener("click",unClic);
 
 function unClic(){
     if (activo==0){
-        iconoHamb.style.backgroundImage = 'url(../icn/cross_menu.png)';
+        iconoHamb.style.backgroundImage = 'url(./icn/cross_menu.png)';
         eventoMenu.style.left = '10%';
         eventoMenu.style.transition = 'left 1s';
         header.style.backgroundColor = 'rgba(0,0,0,0.5)';
         header.style.transition = 'background-color 2s';
         activo = 1
     }else{
-        iconoHamb.style.backgroundImage = 'url(../icn/mobile_menu.png)';
+        iconoHamb.style.backgroundImage = 'url(./icn/mobile_menu.png)';
         eventoMenu.style.left = '-50%'
         eventoMenu.style.transition = 'left 1s' 
         header.style.removeProperty('background-color')
