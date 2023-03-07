776a90a3b3988b7fba2274be1264eb9c24bdac25
Basilio Ossvald <basilio.ossval@gmail.com>
2023-03-07 18:24:21 -0300

    Animación menu mobile

---
+++ js/eventoMenu.js
@@ -0,0 +1,25 @@
+let activo = 0
+let header = document.querySelector("header")
+let eventoMenu = document.querySelector(".navContenido");
+let iconoHamb = document.querySelector(".iconoNav")
+
+iconoHamb.addEventListener("click",unClic);
+
+function unClic(){
+    if (activo==0){
+        iconoHamb.style.backgroundImage = 'url(../icn/cross_menu.png)';
+        eventoMenu.style.left = '10%';
+        eventoMenu.style.transition = 'left 1s';
+        header.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        header.style.transition = 'background-color 2s';
+        activo = 1
+    }else{
+        iconoHamb.style.backgroundImage = 'url(../icn/mobile_menu.png)';
+        eventoMenu.style.left = '-50%'
+        eventoMenu.style.transition = 'left 1s' 
+        header.style.removeProperty('background-color')
+        activo = 0
+    }
+}
+
+
