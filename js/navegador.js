f1f91e049cda7fa159e9b3243a61c3c26fba715a
Basilio Ossvald <basilio.ossval@gmail.com>
2023-04-30 20:17:48 -0300

    nuevo navegador js

--- js/navegador.js
+++ js/navegador.js
@@ -2,6 +2,61 @@ document.body.onload = addNavegador;
 
 
 function addNavegador(){
-    let body = document.querySelector('body');
-    console.log(body)
+    let header = document.querySelector('header');
+    
+    let seccion_logo = document.createElement('section');
+    seccion_logo.classList.add('logo');
+    
+    let a_logo = document.createElement('a');
+    a_logo.href = "../index.html"
+    let img_logo = document.createElement('img')
+    img_logo.src = "/img/logos/Celeste Mobile sin fondo.png"
+
+    header.appendChild(seccion_logo).appendChild(a_logo).appendChild(img_logo)
+     //fin logo navegador
+
+     let iconoNav = document.createElement('section');
+     iconoNav.classList.add('iconoNav')
+
+     header.appendChild(iconoNav)
+    //fin iconoNav navegador
+
+    let nav = document.createElement('nav');
+    nav.classList.add('nav');
+
+    let navContenido = document.createElement('ul');
+    navContenido.classList.add('navContenido');
+
+    let li_clase = document.createElement('li')
+    let li_unete = document.createElement('li')
+    
+    let a_li_clases = document.createElement('a')
+    a_li_clases.href = '/pages/clases.html'
+    a_li_clases.text = 'Clases'
+
+    let a_li_unite = document.createElement('a')
+    a_li_unite.href = '/pages/unete.html'
+    a_li_unite.text = 'Unite al Equipo'
+
+    header.appendChild(nav).appendChild(navContenido).appendChild(li_clase).appendChild(a_li_clases)
+    header.appendChild(nav).appendChild(navContenido).appendChild(li_unete).appendChild(a_li_unite)
+
 }
+
+addNavegador()
+
+
+/*      <section class="logo">
+            <a href="#">
+                <img src="./img/logos/Celeste Mobile sin fondo.png" alt="">
+            </a>
+        </section>
+        <section class="iconoNav">
+        </section>
+        <nav class="nav">
+            <ul class="navContenido">
+                <li><a href="./pages/clases.html">Clases</a></li>
+                <li><a href="./pages/unete.html">Unite al Equipo</a></li>
+                <li><a href="./pages/contactenos.html">Contacto</a></li>
+            </ul>
+        </nav> */
